18d2320348c885ac73a628cda72e7da356d20d57
Merian Lesster <merianlesster27.ml@gmail.com>
2021-03-12 05:12:22 +0530

    Update contactMe.php
    updated php

--- forms/contactMe.php
+++ forms/contactMe.php
@@ -4,10 +4,10 @@
 $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
-$to = " merianlesster27.ml@gmail.com";
+$to = "merianlesster27.ml@gmail.com";
 $subject= $_POST['subject'];
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@merian_portfolio.com" . "\r\n" .
+$headers = "From: lesster2799@gmail.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
